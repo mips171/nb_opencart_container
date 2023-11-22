b6769dc0d82c7fcf7b35037b5ca34baf91a850c5
Nicholas Smith <nicholas@nbembedded.com>
2023-11-22 20:53:59 +1000

    update dockerfile to use caching somewhat

--- 3/debian-11/build_image.sh
+++ 3/debian-11/build_image.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker context use default
-docker system prune -f
-docker buildx build --platform linux/amd64 --build-arg CACHEBUST="$(date +%s)" -t nbembedded/opencart:latest --push .
+# docker system prune -f
+docker buildx build --platform linux/amd64 --build-arg CACHEBUST="$(date +%s)" -t nbembedded/opencart:3.0.3.8-nbembedded-03 --push .
+date
