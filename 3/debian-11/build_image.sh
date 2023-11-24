994ec8ee542a8fe6c79d2d3fb2f63a0a069d5845
Nicholas Smith <nicholas@nbembedded.com>
2023-11-24 15:16:03 +1000

    update tag

--- 3/debian-11/build_image.sh
+++ 3/debian-11/build_image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker context use default
 # docker system prune -f
-docker buildx build --platform linux/amd64 --build-arg CACHEBUST="$(date +%s)" -t nbembedded/opencart:3.0.3.8-nbembedded-03 --push .
+docker buildx build --platform linux/amd64 --build-arg CACHEBUST="$(date +%s)" -t nbembedded/opencart:3.0.3.8-nbembedded-05 --push .
 date
